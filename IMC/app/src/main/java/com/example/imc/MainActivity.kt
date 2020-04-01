a5c73fc86e3939668fc7cfac8c36bf41dd5feb28
leoseixas1 <leonardo.seixas@rede.ulbra.br>
2020-04-01 18:57:20 -0300

    Trabalhos

--- IMC/app/src/main/java/com/example/imc/MainActivity.kt
+++ IMC/app/src/main/java/com/example/imc/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular.setOnClickListener(View.OnClickListener{
+            var idade = edtIdade.text.toString().toInt()
+            var altura = edtIdade.text.toString().toDouble()
+            var peso = edtPeso.text.toString().toDouble()
+            var resultado = ((altura*altura)/peso)
+
+        })
     }
 }
